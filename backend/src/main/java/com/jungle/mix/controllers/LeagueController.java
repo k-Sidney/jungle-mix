bb1e967486bdd7b2fb32c46e35d2dd6adbb85901
k-Sidney <kleubersidney@hotmail.com>
2024-09-25 20:32:28 -0300

    Pagination

--- backend/src/main/java/com/jungle/mix/controllers/LeagueController.java
+++ backend/src/main/java/com/jungle/mix/controllers/LeagueController.java
@@ -1,9 +1,11 @@
 package com.jungle.mix.controllers;
 
 import java.net.URI;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -26,8 +29,14 @@ public class LeagueController {
 	private LeagueService service;
 
 	@GetMapping
-	public ResponseEntity<List<LeagueDTO>> findAll() {
-		List<LeagueDTO> list = service.findAll();
+	public ResponseEntity<Page<LeagueDTO>> findAll(@RequestParam(defaultValue = "0") Integer page,
+			@RequestParam(defaultValue = "12") Integer linesPerPage,
+			@RequestParam(defaultValue = "name") String orderBy,
+			@RequestParam(defaultValue = "ASC") String direction) {
+
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
+		Page<LeagueDTO> list = service.findAllPaged(pageRequest);
 		return ResponseEntity.ok().body(list);
 	}
 
@@ -49,9 +58,9 @@ public class LeagueController {
 		dto = service.update(id, dto);
 		return ResponseEntity.ok().body(dto);
 	}
-	
+
 	@DeleteMapping(value = "/{id}")
-	public ResponseEntity<Void> delete(@PathVariable Long id){
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
--- backend/src/main/java/com/jungle/mix/services/LeagueService.java
+++ backend/src/main/java/com/jungle/mix/services/LeagueService.java
@@ -1,11 +1,11 @@
 package com.jungle.mix.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,8 +25,8 @@ public class LeagueService {
 	private LeagueRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<LeagueDTO> findAll() {
-		return repository.findAll().stream().map(x -> new LeagueDTO(x)).collect(Collectors.toList());
+	public Page<LeagueDTO> findAllPaged(PageRequest pageRequest) {
+		return repository.findAll(pageRequest).map(x -> new LeagueDTO(x));
 
 	}
 
